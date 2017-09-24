f86c6da0cb8a6cdcb33f876b311b9662d265b841
Karthick MP <sigmasix06@gmail.com>
2017-09-24 23:56:42 +0530

    added ORM DB

--- project/_config.py
+++ project/_config.py
@@ -4,10 +4,10 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE = 'flasktaskr.db'
-USERNAME = 'admin'
-PASSWORD = 'admin'
+# USERNAME = 'admin'
+# PASSWORD = 'admin'
 CSRF_ENABLED = True
-SECRET_KEY = 'my_precious'
+SECRET_KEY = 'myprecious'
 
 # define the full path for the database
 DATABASE_PATH = os.path.join(basedir,DATABASE)
